5d3604cada97fc5a3bf919f941eec44f7e3b745f
Max <sibo_jian_29903549@163.com>
2019-03-21 17:15:53 +0800

    [功能]($\Demo & $\Sources)
    添加 Cocoa Touch Framework 项目主体，对项目文件目录结构调整。
    添加 Single View App 项目主体，对项目文件目录结构调整。
    添加 JSAnimatedView.xcworkspace 管理上述项目主体。

---
+++ Demo/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JSAnimatedView-Demo
+//
+//  Created by Max on 2019/3/21.
+//  Copyright © 2019 Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
