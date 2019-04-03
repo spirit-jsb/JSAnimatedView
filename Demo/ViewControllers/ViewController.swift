eccbc5d98843d7450f4378255c28d61fb9af1fd6
Max <sibo_jian_29903549@163.com>
2019-04-03 14:29:08 +0800

    [功能]($Demo)
    编写 JSAnimatedView 示例。

--- Demo/ViewControllers/ViewController.swift
+++ Demo/ViewControllers/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
+import JSAnimatedView
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var animatedImageView: AnimatedImageView!
+    
+    private var currentGIFName: String = "mugen" {
+        didSet {
+            self.animate()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.animate()
     }
 
-
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        self.animatedImageView.stopAnimating()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    private func animate() {
+        self.animatedImageView.animate(withGIFNamed: self.currentGIFName)
+    }
+    
+    @IBAction func toggleAnimation(_ sender: UITapGestureRecognizer) {
+        self.animatedImageView.isAnimating ? self.animatedImageView.stopAnimating() : self.animatedImageView.startAnimating()
+    }
+    
+    @IBAction func swapImage(_ sender: UISwipeGestureRecognizer) {
+        switch self.currentGIFName {
+        case "mugen":
+            self.currentGIFName = "earth"
+        default:
+            self.currentGIFName = "mugen"
+        }
+    }
 }
-
